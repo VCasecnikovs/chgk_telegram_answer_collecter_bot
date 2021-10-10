1989f6bb923200d78114f805d3c39bf3782eafd5
Vadims Casecnikovs (Vadim Chashechnikov) <chashechnikovvadim@gmail.com>
2021-10-10 17:40:33 +0300

    #6 is done

--- game_master.py
+++ game_master.py
@@ -1,22 +1,29 @@
 from enum import Enum
 
-class GameStatus(Enum):
+class GAME_STATUS(Enum):
     IDLE = 0
     REGISTRATION = 1
     IN_GAME = 2
+    ANSWERS_COLLECTION = 3
 
 class GameMaster():
     def __init__(self):
-        self.status : GameStatus = GameStatus.IDLE
+        self.status : GAME_STATUS = GAME_STATUS.IDLE
 
     def start_registration(self):
-        self.status = GameStatus.REGISTRATION
+        self.status = GAME_STATUS.REGISTRATION
     
     def end_registration(self):
-        self.status = GameStatus.IDLE
+        self.status = GAME_STATUS.IDLE
 
     def start_game(self):
-        self.status = GameStatus.IN_GAME
+        self.status = GAME_STATUS.IN_GAME
 
     def end_game(self):
-        self.status = GameStatus.IDLE
+        self.status = GAME_STATUS.IDLE
+
+    def start_answers_collection(self):
+        self.status = GAME_STATUS.ANSWERS_COLLECTION
+
+    def end_answers_collection(self):
+        self.status = GAME_STATUS.IN_GAME
